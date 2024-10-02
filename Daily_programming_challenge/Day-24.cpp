92d2205ec2dc6578a82931ac4be943af74d10e4e
krishh2530 <136615051+krishh2530@users.noreply.github.com>
2024-10-02 15:51:12 +0530

    Add files via upload

---
+++ Daily_programming_challenge/Day-24.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL || root == p || root == q) {
+            return root;
+        }
+        
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        
+        if (left != NULL && right != NULL) {
+            return root;
+        }
+        
+        return left != NULL ? left : right;
+    }
+};
+
+int main() {
+    Solution sol;
+    
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(5);
+    root->right = new TreeNode(1);
+    root->left->left = new TreeNode(6);
+    root->left->right = new TreeNode(2);
+    root->right->left = new TreeNode(0);
+    root->right->right = new TreeNode(8);
+    root->left->right->left = new TreeNode(7);
+    root->left->right->right = new TreeNode(4);
+
+    TreeNode* p = root->left;
+    TreeNode* q = root->right;
+
+    TreeNode* lca = sol.lowestCommonAncestor(root, p, q);
+    cout << "LCA: " << lca->val << endl;
+
+    return 0;
+}
