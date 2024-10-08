32cc2e60c0d9cc28a8b3b57fe7f4212d2ac2c2b3
krishh2530 <136615051+krishh2530@users.noreply.github.com>
2024-10-08 16:45:08 +0530

    Add files via upload

---
+++ Daily_programming_challenge/Day-30.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int coinChange(vector<int>& coins, int amount) {
+    vector<int> dp(amount + 1, amount + 1);
+    dp[0] = 0;
+
+    for (int i = 1; i <= amount; ++i) {
+        for (int coin : coins) {
+            if (i - coin >= 0) {
+                dp[i] = min(dp[i], dp[i - coin] + 1);
+            }
+        }
+    }
+
+    return dp[amount] > amount ? -1 : dp[amount];
+}
+
+int main() {
+    vector<int> coins = {1, 2, 5};
+    int amount = 11;
+    int result = coinChange(coins, amount);
+    
+    if (result != -1) {
+        cout << "Minimum number of coins needed: " << result << endl;
+    } else {
+        cout << "Amount cannot be formed by any combination of coins." << endl;
+    }
+    
+    return 0;
+}
